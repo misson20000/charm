f56f96ac2d5fbc3daf2a03f0fcfdbee375190d44
misson20000 <xenotoad@xenotoad.net>
2023-01-07 23:28:50 -0500

    hexdump token views: maintain pitch even when starting at an offset (after a child)

--- src/view/listing/token_view/hexdump.rs
+++ src/view/listing/token_view/hexdump.rs
@@ -1,5 +1,6 @@
 use crate::model::addr;
 use crate::model::datapath;
+use crate::model::document::structure;
 use crate::model::listing::cursor;
 use crate::view::helpers;
 use crate::view::gsc;
@@ -15,10 +16,15 @@ pub fn render(token_view: &mut TokenView, extent: addr::Extent, snapshot: &gtk::
         cursor::CursorClass::Hexdump(hxc) if has_cursor => Some(hxc),
         _ => None,
     };
-    
-    for i in 0..extent.length().bytes {
-        let byte_record = token_view.data_cache.get(i as usize).copied().unwrap_or(datapath::ByteRecord::default());
 
+    let offset_in_line = match token_view.token.node.props.content_display {
+        structure::ContentDisplay::Hexdump(pitch) => {
+            (extent.begin.to_size() % pitch).bytes as i64
+        },
+        _ => 0
+    };
+    
+    for i in (-offset_in_line)..extent.length().bytes as i64 {
         if i != 0 {
             /* insert a gutter between every byte */
             pos.set_x(pos.x() + helpers::pango_unscale(render.gsc_mono.space_width()));
@@ -28,10 +34,18 @@ pub fn render(token_view: &mut TokenView, extent: addr::Extent, snapshot: &gtk::
                 pos.set_x(pos.x() + helpers::pango_unscale(render.gsc_mono.space_width()));
             }
         }
+
+        if i < 0 {
+            pos.set_x(pos.x() + helpers::pango_unscale(render.gsc_mono.space_width()));
+            pos.set_x(pos.x() + helpers::pango_unscale(render.gsc_mono.space_width()));
+            continue;
+        }
+        
+        let byte_record = token_view.data_cache.get(i as usize).copied().unwrap_or(datapath::ByteRecord::default());
         
         for low_nybble in [false, true] {
             let nybble = if low_nybble { byte_record.value & 0xf } else { byte_record.value >> 4 };
-            let has_cursor = hex_cursor.map_or(false, |hxc| hxc.offset.bytes == i && hxc.low_nybble == low_nybble);
+            let has_cursor = hex_cursor.map_or(false, |hxc| hxc.offset.bytes == i as u64 && hxc.low_nybble == low_nybble);
 
             let digit = gsc::Entry::Digit(nybble);
 
