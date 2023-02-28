05fe07282f1e03be9f1cb0b57f28b0c03a4c3bb2
misson20000 <xenotoad@xenotoad.net>
2023-02-27 23:43:01 -0500

    let property editor change title_display, children_display, and content_display

--- src/view/props_editor.rs
+++ src/view/props_editor.rs
@@ -3,6 +3,7 @@ use std::rc::Rc;
 use std::sync;
 
 use gtk::prelude::*;
+use gtk::gio;
 use gtk::glib;
 use gtk::glib::clone;
 
@@ -27,7 +28,17 @@ pub struct PropsEditor {
     pub toplevel: gtk::Widget,
 
     name_entry: gtk::Entry,
-    children_display: gtk::ComboBox,
+    size_entry: gtk::Entry,
+    
+    title_display: gtk::DropDown,
+    children_display: gtk::DropDown,
+    content_display: gtk::DropDown,
+
+    title_model: gtk::StringList,
+    children_model: gtk::StringList,
+    content_model: gtk::StringList,
+    
+    locked: gtk::CheckButton,
     path_display: gtk::Entry,
 
     in_update: cell::Cell<bool>,
@@ -39,14 +50,33 @@ impl PropsEditor {
         let builder = gtk::Builder::from_string(include_str!("props-editor.ui"));
 
         let toplevel: gtk::Widget = builder.object("toplevel").unwrap();
+        
         let name_entry: gtk::Entry = builder.object("name_entry").unwrap();
-        let children_display: gtk::ComboBox = builder.object("children_display").unwrap();
+        let size_entry: gtk::Entry = builder.object("size_entry").unwrap();
+        let title_display: gtk::DropDown = builder.object("title_display").unwrap();
+        let children_display: gtk::DropDown = builder.object("children_display").unwrap();
+        let content_display: gtk::DropDown = builder.object("content_display").unwrap();
+        let locked: gtk::CheckButton = builder.object("locked").unwrap();
         let path_display: gtk::Entry = builder.object("path_display").unwrap();
 
+        let title_model = gtk::StringList::new(&["Inline", "Major", "Minor"]);
+        let children_model = gtk::StringList::new(&["Hidden", "Summary", "Full"]);
+        let content_model = gtk::StringList::new(&["Hidden", "Hexdump", "Hexstring"]);
+        
         let pe = PropsEditor {
             toplevel,
             name_entry,
+            size_entry,
+            
+            title_display,
             children_display,
+            content_display,
+
+            title_model,
+            children_model,
+            content_model,
+            
+            locked,
             path_display,
             in_update: cell::Cell::new(false),
             interior: cell::RefCell::new(None),
@@ -57,22 +87,54 @@ impl PropsEditor {
         let pe = Rc::new(pe);
 
         pe.name_entry.buffer().connect_text_notify(clone!(@weak pe => move |buffer| {
-            if !pe.in_update.get() {
-                let mut interior_guard = pe.interior.borrow_mut();
-                if let Some(interior) = interior_guard.as_mut() {
-                    if let Some((path, props)) = interior.current.as_mut() {
-                        props.name = buffer.text();
-                        if let Err(e) = interior.document_host.change(interior.selection.document.alter_node(path.clone(), props.clone())) {
-                            println!("failed to alter node: {:?}", e);
-                        }
-                    }
-                }
-            }
+            pe.change_prop(|props| props.name = buffer.text());
+        }));
+
+        pe.title_display.connect_selected_notify(clone!(@weak pe => move |dd| {
+            pe.change_prop(|props| props.title_display = match dd.selected() {
+                0 => structure::TitleDisplay::Inline,
+                1 => structure::TitleDisplay::Major,
+                2 => structure::TitleDisplay::Minor,
+                x => panic!("unexpected selected index: {}", x)
+            });
+        }));
+
+        pe.children_display.connect_selected_notify(clone!(@weak pe => move |dd| {
+            pe.change_prop(|props| props.children_display = match dd.selected() {
+                0 => structure::ChildrenDisplay::None,
+                1 => structure::ChildrenDisplay::Summary,
+                2 => structure::ChildrenDisplay::Full,
+                x => panic!("unexpected selected index: {}", x)
+            });
+        }));
+
+        pe.content_display.connect_selected_notify(clone!(@weak pe => move |dd| {
+            pe.change_prop(|props| props.content_display = match dd.selected() {
+                0 => structure::ContentDisplay::None,
+                1 => structure::ContentDisplay::Hexdump(16.into()),
+                2 => structure::ContentDisplay::Hexstring,
+                x => panic!("unexpected selected index: {}", x)
+            });
         }));
         
         pe
     }
 
+    fn change_prop<F: FnOnce(&mut structure::Properties)>(&self, cb: F) {
+        if !self.in_update.get() {
+            let mut interior_guard = self.interior.borrow_mut();
+            if let Some(interior) = interior_guard.as_mut() {
+                if let Some((path, props)) = interior.current.as_mut() {
+                    cb(props);
+                    
+                    if let Err(e) = interior.document_host.change(interior.selection.document.alter_node(path.clone(), props.clone())) {
+                        println!("failed to alter node: {:?}", e);
+                    }
+                }
+            }
+        }        
+    }
+    
     pub fn unbind(&self) {
         self.interior.take();
         self.name_entry.set_sensitive(false);
@@ -135,14 +197,49 @@ impl PropsEditor {
     fn update_controls(&self, props: Option<&structure::Properties>) {
         if let Some(props) = props {
             self.name_entry.set_text(&props.name);
+
+            self.title_display.set_model(Some(&self.title_model));
+            self.title_display.set_selected(match props.title_display {
+                structure::TitleDisplay::Inline => 0,
+                structure::TitleDisplay::Major => 1,
+                structure::TitleDisplay::Minor => 2,
+            });
+
+            self.children_display.set_model(Some(&self.children_model));
+            self.children_display.set_selected(match props.children_display {
+                structure::ChildrenDisplay::None => 0,
+                structure::ChildrenDisplay::Summary => 1,
+                structure::ChildrenDisplay::Full => 2,
+            });
+
+            self.content_display.set_model(Some(&self.content_model));
+            self.content_display.set_selected(match props.content_display {
+                structure::ContentDisplay::None => 0,
+                structure::ContentDisplay::Hexdump(_) => 1,
+                structure::ContentDisplay::Hexstring => 2,
+            });
+            
+            self.locked.set_active(props.locked);
             
             self.name_entry.set_sensitive(true);
+            self.size_entry.set_sensitive(true);
+            self.title_display.set_sensitive(true);
             self.children_display.set_sensitive(true);
+            self.content_display.set_sensitive(true);
+            self.locked.set_sensitive(true);
         } else {
             self.name_entry.set_text("");
+            self.title_display.set_model(gio::ListModel::NONE);
+            self.children_display.set_model(gio::ListModel::NONE);
+            self.content_display.set_model(gio::ListModel::NONE);
+            self.locked.set_active(false);
             
             self.name_entry.set_sensitive(false);
+            self.size_entry.set_sensitive(false);
+            self.title_display.set_sensitive(false);
             self.children_display.set_sensitive(false);
+            self.content_display.set_sensitive(false);
+            self.locked.set_sensitive(false);
         }
     }
 
