5c59dea8f3eb5c55df128fd0ffcd12e5d7420e05
misson20000 <xenotoad@xenotoad.net>
2023-04-30 15:11:39 -0400

    first pass at porting ListingSelection across document changes

--- src/model/selection/listing.rs
+++ src/model/selection/listing.rs
@@ -102,8 +102,17 @@ impl Selection {
         }
     }
 
-    fn port_doc_change(&mut self, _new_doc: &sync::Arc<document::Document>, _change: &doc_change::Change) {
-        todo!();
+    fn port_doc_change(&mut self, new_doc: &sync::Arc<document::Document>, change: &doc_change::Change) {
+        self.document = new_doc.clone();
+
+        self.mode = match std::mem::replace(&mut self.mode, Mode::Structure(StructureMode::Empty)) {
+            Mode::Structure(sm) => Mode::Structure(match sm {
+                StructureMode::Empty => StructureMode::Empty,
+                StructureMode::Range(sr) => sr.port_doc_change(new_doc, change),
+                StructureMode::All => StructureMode::All,
+            }),
+            Mode::Address(extent) => Mode::Address(extent),
+        };
     }
 }
 
@@ -123,9 +132,129 @@ impl versioned::Change<Selection> for Change {
     type ApplyError = ApplyError;
     type ApplyRecord = ChangeRecord;
 
-    #[allow(unused_mut)] // TODO: remove me once this is implemented
-    fn apply(mut self, _selection: &mut Selection) -> Result<(Change, ChangeRecord), ApplyError> {
-        todo!();
+    fn apply(mut self, selection: &mut Selection) -> Result<(Change, ChangeRecord), ApplyError> {
+        let record = match &mut self {
+            Change::DocumentUpdated(new_document) => selection.update_document(new_document),
+            _ => todo!(),
+        };
+
+        Ok((self, record))
+    }
+}
+
+impl StructureRange {
+    fn extent(&self) -> addr::Extent {
+        addr::Extent::between(self.begin.0, self.end.0)
+    }
+    
+    fn port_doc_change(mut self, new_doc: &sync::Arc<document::Document>, change: &doc_change::Change) -> StructureMode {
+        let ret = match change.update_path(&mut self.path) {
+            doc_change::UpdatePathResult::Moved | doc_change::UpdatePathResult::Unmoved => StructureMode::Range(match &change.ty {
+                doc_change::ChangeType::AlterNode(_, _) => self,
+                doc_change::ChangeType::InsertNode(affected_path, insertion_index, childhood) if affected_path == &self.path => {
+                    if self.begin.1 > *insertion_index || (self.begin.1 == *insertion_index && self.begin.0 > childhood.offset) {
+                        self.begin.1+= 1;
+                    }
+                    
+                    if self.end.1 >= *insertion_index {
+                        self.end.1+= 1;
+                    }
+                    
+                    if childhood.extent().includes(self.begin.0) {
+                        self.begin.0 = childhood.offset;
+                    }
+                    
+                    if childhood.extent().includes(self.end.0) {
+                        self.end.0 = childhood.end();
+                    }
+                    
+                    self
+                }
+                doc_change::ChangeType::InsertNode(_, _, _) => self,
+                doc_change::ChangeType::Nest(affected_path, nested_first, nested_last, nested_extent, _props) if affected_path == &self.path => {
+                    let nested = *nested_first..=*nested_last;
+                    let new_child = &new_doc.lookup_node(affected_path).0.children[*nested_first];
+                        
+                    if nested.contains(&self.begin.1) && nested.contains(&self.end.1) && nested_extent.contains(self.extent()) {
+                        /* The entire range was nested, so just select the same range in the child. */
+                        self.path.push(*nested_first);
+                        self.begin.0-= new_child.offset.to_size();
+                        self.begin.1-= *nested_first;
+                        self.end.1-= *nested_first;
+                    } else {
+                        /* If the beginning was part of the nested range, adjust it to the beginning of the nest. */
+                        if nested_extent.includes(self.begin.0) {
+                            self.begin.0 = nested_extent.begin;
+                        }
+
+                        if nested.contains(&self.begin.1) {
+                            self.begin.1 = *nested_first;
+                        } else if self.begin.1 > *nested_last {
+                            self.begin.1-= nested_last-nested_first;
+                        }
+
+                        /* If the end was part of the nested range, adjust it to the end of the nest. */
+                        if nested_extent.includes(self.end.0) {
+                            self.end.0 = nested_extent.end;
+                        }
+
+                        if nested.contains(&self.end.1) {
+                            self.end.1 = nested_first + 1;
+                        } else if self.end.1 > *nested_last {
+                            self.end.1-= nested_last-nested_first;
+                        }
+                    }
+
+                    self
+                },
+                doc_change::ChangeType::Nest(_, _, _, _, _) => self,
+                doc_change::ChangeType::DeleteRange(affected_path, deleted_first, deleted_last) if affected_path == &self.path => {
+                    let deleted = *deleted_first..=*deleted_last;
+                    if deleted.contains(&self.begin.1) && deleted.contains(&self.end.1) {
+                        return StructureMode::Empty;
+                    } else {
+                        if deleted.contains(&self.begin.1) {
+                            self.begin.1 = *deleted_first;
+                        } else if self.begin.1 > *deleted_last {
+                            self.begin.1-= (*deleted_last-*deleted_first)+1;
+                        }
+                        
+                        if deleted.contains(&self.end.1) {
+                            self.end.1 = *deleted_first-1;
+                        } else if self.end.1 > *deleted_last {
+                            self.end.1-= (*deleted_last-*deleted_first)+1;
+                        }
+
+                        self
+                    }
+                },
+                doc_change::ChangeType::DeleteRange(_, _, _) => self,
+            }),
+            doc_change::UpdatePathResult::Deleted => StructureMode::Empty,
+        };
+
+        ret.check_integrity(new_doc);
+
+        ret
+    }
+
+    fn check_integrity(&self, document: &document::Document) {
+        let node = document.lookup_node(&self.path).0;
+        assert!(self.begin.0 <= node.children[self.begin.1].offset);
+        if self.end.1 != node.children.len() {
+            assert!(self.end.0 >= node.children[self.end.1].end());
+        }
+        assert!(self.end.0 <= node.size.to_addr());
+    }
+}
+
+impl StructureMode {
+    fn check_integrity(&self, document: &document::Document) {
+        match self {
+            StructureMode::Empty => {},
+            StructureMode::Range(sr) => sr.check_integrity(document),
+            StructureMode::All => {},
+        }
     }
 }
 
