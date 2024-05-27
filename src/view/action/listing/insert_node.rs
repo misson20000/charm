9bcf0840fa35ef1406216d9626775b4ea250d5dd
misson20000 <xenotoad@xenotoad.net>
2024-05-27 15:27:48 -0400

    Remove nest nodes action and add nesting option to insert node action dialog box

--- src/view/action/listing/insert_node.rs
+++ src/view/action/listing/insert_node.rs
@@ -31,19 +31,8 @@ struct InsertNodeAction {
     offset_entry: gtk::Entry,
     order_entry: gtk::DropDown,
     path_display: gtk::Entry,
-}
-
-struct NestNodesAction {
-    document_host: sync::Arc<document::DocumentHost>,
-    activation: cell::RefCell<Option<NestActivation>>,
-
-    lw: listing::ListingWidget,
-    window: rc::Weak<window::CharmWindow>,
-    
-    dialog: gtk::ApplicationWindow,
-    
-    name_entry: gtk::Entry,
-    path_display: gtk::Entry,
+    nest_enable: gtk::CheckButton,
+    nest_entry: gtk::DropDown,
 }
 
 struct InsertActivation {
@@ -51,29 +40,16 @@ struct InsertActivation {
     path: structure::Path,
 }
 
-struct NestActivation {
-    document: sync::Arc<document::Document>,
-    range: selection::listing::StructureRange,
-}
-
 pub fn add_actions(window_context: &window::WindowContext) {
     let insert_action = InsertNodeAction::new(window_context);
-    let nest_action   =  NestNodesAction::new(window_context);
-    let insert_action_clone = insert_action.clone();
-
     let insert_gio_action = gio::SimpleAction::new("insert_node", None);
 
     insert_gio_action.connect_activate(move |_, _| catch_panic! {
-        if nest_action.activate() {
-        } else {
-            insert_action.activate();
-        }
+        insert_action.activate();
     });
     
     insert_gio_action.set_enabled(true);
-
     window_context.action_group.add_action(&insert_gio_action);
-    window_context.action_group.add_action(&helpers::create_simple_action_strong(insert_action_clone, "force_insert_node", |ina| { ina.activate(); }));
 }
 
 impl InsertNodeAction {
@@ -85,8 +61,12 @@ impl InsertNodeAction {
         let offset_entry: gtk::Entry = builder.object("offset_entry").unwrap();
         let order_entry: gtk::DropDown = builder.object("order_entry").unwrap();
         let path_display: gtk::Entry = builder.object("path_display").unwrap();
+        let nest_enable: gtk::CheckButton = builder.object("nest_enable").unwrap();
+        let nest_entry: gtk::DropDown = builder.object("nest_entry").unwrap();
         let insert_button: gtk::Button = builder.object("insert_button").unwrap();
 
+        nest_enable.bind_property("active", &nest_entry, "sensitive").sync_create().build();
+        
         let dialog = gtk::ApplicationWindow::builder()
             .application(&window_context.window.upgrade().unwrap().application.application)
             .child(&builder.object::<gtk::Widget>("toplevel").unwrap())
@@ -110,6 +90,8 @@ impl InsertNodeAction {
             offset_entry,
             order_entry,
             path_display,
+            nest_enable,
+            nest_entry,
         });
 
         helpers::bind_simple_action(&action, &action.dialog, "cancel", |action| {
@@ -130,68 +112,80 @@ impl InsertNodeAction {
 
         action
     }
-    
-    fn do_insert(&self) {
-        if let Some(window) = self.window.upgrade() {
-            let name = self.name_entry.text().as_str().to_string();
 
-            let size_text = self.size_entry.text();
-            let size = match addr::Address::parse(size_text.as_str()) {
-                Ok(a) => a,
-                Err(e) => {
-                    window.report_error(error::Error {
-                        while_attempting: error::Action::InsertNodeParseSize,
-                        trouble: error::Trouble::AddressParseFailed {
-                            error: e,
-                            address: size_text.to_string(),
-                        },
-                        level: error::Level::Error,
-                        is_bug: false,
-                    });
-                    return;
-                }
-            }.to_size();
+    fn change(&self) -> Result<document::change::Change, error::Error> {
+        let name = self.name_entry.text().as_str().to_string();
 
-            let offset_text = self.offset_entry.text();
-            let offset = match addr::Address::parse(offset_text.as_str()) {
-                Ok(a) => a,
-                Err(e) => {
-                    window.report_error(error::Error {
-                        while_attempting: error::Action::InsertNodeParseOffset,
-                        trouble: error::Trouble::AddressParseFailed {
-                            error: e,
-                            address: offset_text.to_string(),
-                        },
-                        level: error::Level::Error,
-                        is_bug: false,
-                    });
-                    return;
-                }
-            };
+        let size_text = self.size_entry.text();
+        let size = match addr::Address::parse(size_text.as_str()) {
+            Ok(a) => a,
+            Err(e) => return Err(error::Error {
+                while_attempting: error::Action::InsertNodeParseSize,
+                trouble: error::Trouble::AddressParseFailed {
+                    error: e,
+                    address: size_text.to_string(),
+                },
+                level: error::Level::Error,
+                is_bug: false,
+            })
+        }.to_size();
 
-            let activation = match self.activation.take() {
-                Some(a) => a,
-                None => {
-                    /* This shouldn't happen. */
-                    return;
-                }
-            };
-            
-            let parent_node = activation.document.lookup_node(&activation.path).0;
-            let index = self.order_entry.selected() as usize;
+        let offset_text = self.offset_entry.text();
+        let offset = match addr::Address::parse(offset_text.as_str()) {
+            Ok(a) => a,
+            Err(e) => return Err(error::Error {
+                while_attempting: error::Action::InsertNodeParseOffset,
+                trouble: error::Trouble::AddressParseFailed {
+                    error: e,
+                    address: offset_text.to_string(),
+                },
+                level: error::Level::Error,
+                is_bug: false,
+            })
+        };
 
-            if let Err((error, attempted_version)) = self.document_host.change(activation.document.insert_node(
+        let activation = match self.activation.take() {
+            Some(a) => a,
+            None => {
+                /* This shouldn't happen. */
+                panic!("Insert node action running without activation");
+            }
+        };
+        
+        let parent_node = activation.document.lookup_node(&activation.path).0;
+        let props = parent_node.props.clone_rename(name);
+        let index = self.order_entry.selected() as usize;
+
+        if self.nest_enable.is_active() {
+            Ok(activation.document.nest(
+                structure::SiblingRange::new(activation.path.clone(), index, self.nest_entry.selected() as usize),
+                addr::Extent::sized(offset, size),
+                props,
+            ))
+        } else {
+            Ok(activation.document.insert_node(
                 activation.path,
                 index,
                 structure::Childhood::new(
                     sync::Arc::new(structure::Node {
-                        props: parent_node.props.clone_rename(name),
+                        props,
                         children: Vec::new(),
                         size,
                     }),
                     offset
                 )
-            )) {
+            ))
+        }
+    }
+    
+    fn do_insert(&self) {
+        if let Some(window) = self.window.upgrade() {
+            let change = match self.change() {
+                Ok(c) => c,
+                Err(e) => return window.report_error(e)
+            };
+            
+            if let Err((error, attempted_version)) = self.document_host.change(change) {
                 /* Inform the user that their action failed. */
                 window.report_error(error::Error {
                     while_attempting: error::Action::InsertNode,
@@ -206,7 +200,7 @@ impl InsertNodeAction {
         }
     }
     
-    pub fn activate(&self) -> bool {
+    pub fn activate(&self) {
         let selection = self.lw.selection();
 
         match &selection.mode {
@@ -216,7 +210,14 @@ impl InsertNodeAction {
                     range.path.clone(),
                     range.begin.0,
                     range.begin.1,
-                    Some(range.extent().length()))
+                    Some(range.extent().length()),
+
+                    /* convert StructureRange's [begin, end) interval to inclusive Option<[begin, end]> interval */
+                    if range.begin.1 == range.end.1 {
+                        None
+                    } else {
+                        Some(range.end.1 - 1)
+                    })
             },
 
             _ => {
@@ -227,14 +228,21 @@ impl InsertNodeAction {
                     cursor.structure_path(),
                     cursor.structure_offset(),
                     cursor.structure_child_index(),
+                    None,
                     None)
             }
         }
-
-        true
     }
 
-    fn activate_impl(&self, document: sync::Arc<document::Document>, path: structure::Path, offset: addr::Address, index: usize, size: Option<addr::Size>) {
+    fn activate_impl(
+        &self,
+        document: sync::Arc<document::Document>,
+        path: structure::Path,
+        offset: addr::Address,
+        index: usize,
+        size: Option<addr::Size>,
+        nest_through: Option<usize>) {
+        
         let activation = InsertActivation {
             document,
             path,
@@ -245,13 +253,31 @@ impl InsertNodeAction {
         self.path_display.set_text(&activation.document.describe_path(&activation.path));
 
         let (node, _) = activation.document.lookup_node(&activation.path);
-        let model = gtk::StringList::new(&[]);
-        for i in &node.children {
-            model.append(&i.node.props.name);
+
+        {
+            let model = gtk::StringList::new(&[]);
+            for i in &node.children {
+                model.append(&i.node.props.name);
+            }
+            model.append("<end>");
+            self.order_entry.set_model(Some(&model));
+            self.order_entry.set_selected(index as u32);
         }
-        model.append("<end>");
-        self.order_entry.set_model(Some(&model));
-        self.order_entry.set_selected(index as u32);
+
+        self.nest_enable.set_active(nest_through.is_some());
+        {
+            let model = gtk::StringList::new(&[]);
+            for i in &node.children {
+                model.append(&i.node.props.name);
+            }
+            
+            self.nest_entry.set_model(Some(&model));
+            if let Some(nt) = nest_through {
+                self.nest_entry.set_selected(nt as u32);
+            } else {
+                self.nest_entry.set_selected(node.children.len() as u32);
+            }
+        }
         
         self.activation.replace(Some(activation));
         
@@ -267,145 +293,6 @@ impl InsertNodeAction {
 }
 
 impl Drop for InsertNodeAction {
-    fn drop(&mut self) {
-        self.dialog.destroy();
-    }
-}
-
-impl NestNodesAction {
-    fn new(window_context: &window::WindowContext) -> rc::Rc<Self> {
-        let builder = gtk::Builder::from_string(include_str!("nest-nodes.ui"));
-
-        let name_entry: gtk::Entry = builder.object("name_entry").unwrap();
-        let path_display: gtk::Entry = builder.object("path_display").unwrap();
-        let nest_button: gtk::Button = builder.object("nest_button").unwrap();
-
-        let dialog = gtk::ApplicationWindow::builder()
-            .application(&window_context.window.upgrade().unwrap().application.application)
-            .child(&builder.object::<gtk::Widget>("toplevel").unwrap())
-            .resizable(true)
-            .title("Nest nodes")
-            .transient_for(&window_context.window.upgrade().unwrap().window)
-            .hide_on_close(true)
-            .destroy_with_parent(true)
-            .default_widget(&nest_button)
-            .build();
-        
-        let action = rc::Rc::new(NestNodesAction {
-            document_host: window_context.project.document_host.clone(),
-            activation: cell::RefCell::new(None),
-            lw: window_context.lw.clone(),
-            window: window_context.window.clone(),
-            dialog: dialog.clone(),
-            
-            name_entry,
-            path_display,
-        });
-        
-        helpers::bind_simple_action(&action, &action.dialog, "cancel", |action| {
-            action.deactivate();
-        });
-
-        helpers::bind_simple_action(&action, &action.dialog, "nest", |action| {
-            action.do_nest();
-            action.deactivate();
-        });
-
-        dialog.connect_close_request(clone!(@weak action => @default-return glib::Propagation::Proceed, move |_| {
-            catch_panic!{ 
-                action.deactivate();
-            };
-            glib::Propagation::Proceed
-        }));
-
-        action
-    }
-    
-    fn do_nest(&self) {
-        if let Some(window) = self.window.upgrade() {
-            let name = self.name_entry.text().as_str().to_string();
-
-            let activation = match self.activation.take() {
-                Some(a) => a,
-                None => {
-                    /* This shouldn't happen. */
-                    return;
-                }
-            };
-            
-            let parent_node = activation.document.lookup_node(&activation.range.path).0;
-
-            let change = match activation.range.to_sibling_range_and_extent() {
-                Ok((sibling_range, extent)) => activation.document.nest(
-                    sibling_range,
-                    extent,
-                    parent_node.props.clone_rename(name)
-                ),
-                Err(sr) => {
-                    let extent = sr.extent();
-                    activation.document.insert_node(
-                        sr.path,
-                        sr.begin.1,
-                        structure::Childhood::new(
-                            sync::Arc::new(structure::Node {
-                                props: parent_node.props.clone_rename(name),
-                                children: Vec::new(),
-                                size: extent.length(),
-                            }),
-                            sr.begin.0
-                        )
-                    )
-                }
-            };
-            
-            if let Err((error, attempted_version)) = self.document_host.change(change) {
-                /* Inform the user that their action failed. */
-                window.report_error(error::Error {
-                    while_attempting: error::Action::NestNodesInListing,
-                    trouble: error::Trouble::DocumentUpdateFailure {
-                        error,
-                        attempted_version
-                    },
-                    level: error::Level::Error,
-                    is_bug: false,
-                });
-            }
-        }
-    }
-    
-    pub fn activate(&self) -> bool {
-        let selection = self.lw.selection();
-
-        match &selection.mode {
-            selection::listing::Mode::Structure(selection::listing::StructureMode::Range(range)) => {
-                self.activate_impl(selection.document.clone(), range.clone());
-                true
-            },
-
-            _ => false
-        }
-    }
-
-    fn activate_impl(&self, document: sync::Arc<document::Document>, range: selection::listing::StructureRange) {
-        let activation = NestActivation {
-            document,
-            range,
-        };
-
-        self.path_display.set_text(&activation.document.describe_path(&activation.range.path));
-        self.activation.replace(Some(activation));
-        
-        self.name_entry.grab_focus();
-        self.dialog.present();
-    }
-    
-    fn deactivate(&self) {
-        self.activation.take();
-        self.dialog.hide();
-    }
-}
-
-impl Drop for NestNodesAction {
     fn drop(&mut self) {
         self.dialog.destroy();
     }
